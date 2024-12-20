a055e9d2912822818ad0df022fa2ce5e5f5278b9
infiniteloops-hue <itsmewajihaa@gmail.com>
2024-12-20 21:01:25 +0500

    Create data_types_example.py

---
+++ data_types_example.py
@@ -0,0 +1,16 @@
+# Data types in Python
+integer_var = 10
+float_var = 20.5
+string_var = "Hello, Python!"
+list_var = [1, 2, 3]
+tuple_var = (4, 5, 6)
+set_var = {7, 8, 9}
+dict_var = {"key": "value"}
+
+print(type(integer_var))
+print(type(float_var))
+print(type(string_var))
+print(type(list_var))
+print(type(tuple_var))
+print(type(set_var))
+print(type(dict_var))
